e565f50d896143e9d71a2f769cf45122a6a61253
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-09-02 21:15:22 -0500

    Added permutation

--- easy/Length_Of_Last_Word.go
+++ easy/Length_Of_Last_Word.go
@@ -96,3 +96,49 @@ func isPalindrome(s string) bool {
 	}
 	return true
 }
+
+/*
+567. Permutation in String
+Solved
+Medium
+Topics
+Companies
+Hint
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+*/
+
+func checkInclusion(s1 string, s2 string) bool {
+	set1 := [26]int{}
+	for _, char := range s1 {
+		set1[char-97]++
+	}
+	set2 := [26]int{}
+	l := 0
+	for index, char := range s2 {
+		set2[char-97]++
+
+		if index >= len(s1) {
+			remChar := s2[l]
+			set2[remChar-97]--
+			l++
+		}
+		if set1 == set2 {
+			return true
+		}
+	}
+	return false
+}
